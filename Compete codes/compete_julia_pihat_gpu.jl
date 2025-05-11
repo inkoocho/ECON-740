a7eee69567c574d61e78c88781e529812cf28221
jay9209 <jung.jae.kim@emory.edu>
2025-05-11 15:09:11 -0400

    update

--- Compete codes/compete_julia_pihat_gpu.jl
+++ Compete codes/compete_julia_pihat_gpu.jl
@@ -1,98 +1,136 @@
+##
+##    Written by Jung Jae Kim on March 24, 2025. GPU execution code of compete_julia_pihat_benchmark.jl
+##
+
+# Maximum bound of the simulation
+# Nsim = 400, Numf = 10, Numb = 10, Titer = 10000
+
 using CUDA, Random, Distributions, Plots
 
-function compete_kernel!(price, pi1, pihat1, beta0, beta1, vf_vals, vp_vals, Nsim, Titer, alpha, rho, sigmap, sigmaf, sigmab)
-    i = threadIdx().x + (blockIdx().x - 1) * blockDim().x  # Compute thread index
-    if i > Nsim return end  # Ensure within range
+# **Setup Parameters**
+Nsim = 400
+Titer = 10000
 
-    # Initialize per-simulation variables
-    delta=0.8
-    gam = (1-alpha)
-    sce = delta/(1-alpha*rho)
+sigmafmin = 0.0000000001
+sigmafmax = 0.000001
+Numf = 10
+sigmaf_cpu = range(sigmafmin, stop=sigmafmax, length=Numf) |> collect
+sigmaf_grid = CuArray(sigmaf_cpu)
 
-    Vbar=sqrt(sigmap*sigmab*(1-rho^2)/sigmaf)
+sigmabmin = 0.000001
+sigmabmax = 0.01
+Numb = 10 
+sigmab_cpu = range(sigmabmin, stop=sigmabmax, length=Numb) |> collect
+sigmab_grid = CuArray(sigmab_cpu)
 
-    V0 = 1.0*Vbar
+alpha = 0.96
+rho = 0.99
+sigmap = 0.2
+
+TotalSim = Nsim * Numf * Numb
+
+function compete_kernel!(
+    price, pi1, pihat1, beta0, beta1,
+    sigmaf_grid, sigmab_grid,
+    Nsim, Ns, Nb, Titer,
+    alpha, rho, sigmap
+)
+    id = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+    TotalSim = Nsim * Ns * Nb
+    if id > TotalSim
+        return
+    end
+
+    # Unpack simulation indices
+    i_sim = (id - 1) % Nsim + 1
+    i_sf  = ((id - 1) ÷ Nsim) % Ns + 1
+    i_sb  = ((id - 1) ÷ (Nsim * Ns)) % Nb + 1
+
+    # Retrieve sigma values for this thread
+    sigmaf = sigmaf_grid[i_sf]
+    sigmab = sigmab_grid[i_sb]
+
+    # Initialize parameters
+    delta = 0.8
+    gam = (1 - alpha)
+    sce = delta / (1 - alpha * rho)
+    Vbar = sqrt(sigmap * sigmab * (1 - rho^2) / sigmaf)
+
+    V0 = 1.0 * Vbar
     V1 = Vbar
 
-    price_i = gam/(1-alpha)
-    beta0_i = gam/(1-alpha) + .01*vp_vals[i,1]*sqrt(V0)
-    beta1_i = price_i + .01*vp_vals[i,2]*sqrt(V1)
+    price_i = gam / (1 - alpha)
+    beta0_i = gam / (1 - alpha) + 0.01 * randn() * sqrt(V0)
+    beta1_i = price_i + 0.01 * randn() * sqrt(V1)
     pi1_i = 0.5
     pihat1_i = 0.5
 
-    g=sqrt(sigmab*sigmaf)/sqrt(sigmap*(1-rho^2))
-    f = sqrt(sigmaf)*vf_vals[i,1]
-    A1=1
-    A0=1
-    r=0.0
+    g = sqrt(sigmab * sigmaf) / sqrt(sigmap * (1 - rho^2))
+    f = sqrt(sigmaf) * randn()
+    A1 = 1.0
+    A0 = 1.0
+    r = 0.0
 
-    price[i, 1] = price_i
-    pi1[i, 1] = pi1_i
-    pihat1[i, 1] = pihat1_i
-    beta0[i, 1] = beta0_i
-    beta1[i, 1] = beta1_i
+    # Save initial values
+    price[i_sim, i_sf, i_sb, 1] = price_i
+    pi1[i_sim, i_sf, i_sb, 1] = pi1_i
+    pihat1[i_sim, i_sf, i_sb, 1] = pihat1_i
+    beta0[i_sim, i_sf, i_sb, 1] = beta0_i
+    beta1[i_sim, i_sf, i_sb, 1] = beta1_i
 
     for t = 2:Titer
-        vp = sqrt(sigmap)*vf_vals[i, t]
-        vf = sqrt(sigmaf)*vp_vals[i, t]
+        vp = sqrt(sigmap) * randn()
+        vf = sqrt(sigmaf) * randn()
         
-        res1 = price_i - beta1_i 
-        res0 = price_i - (pihat1_i * beta1_i + (1 - pihat1_i)*(beta0_i + sce*f))
+        res1 = price_i - beta1_i
+        res0 = price_i - (pihat1_i * beta1_i + (1 - pihat1_i) * (beta0_i + sce * f))
         f = rho * f + vf
 
-        beta1_i = beta1_i + (V1 / (sigmap + V1))*res1
+        beta1_i += (V1 / (sigmap + V1)) * res1
+        A1 = exp(-0.5 * (res1^2) / (sigmap + V1)) / sqrt(2 * 3.14159 * (sigmap + V1))
 
-        A1 = (exp(-.5 * (res1^2) / (sigmap + V1))) / sqrt(2*3.14159*(sigmap + V1))
-
-        M0gaincp = V0 / (sigmap + V0)
-
-        beta0_i = beta0_i +M0gaincp*res0
-
-        A0 = (exp(-.5 * (res0^2) / (sigmap + V0))) / sqrt(2*3.14159*(sigmap + V0))
+        gain0 = V0 / (sigmap + V0)
+        beta0_i += gain0 * res0
+        A0 = exp(-0.5 * (res0^2) / (sigmap + V0)) / sqrt(2 * 3.14159 * (sigmap + V0))
 
         V1 = V1 - (V1^2) / (sigmap + V1) + sigmab
         V0 = V0 - (V0^2) / (sigmap + V0)
 
-        r = r + log(A1/A0)
+        r += log(A1 / A0)
         pi1_i = 1 / (1 + exp(-r))
-        pihat1_i = pihat1_i + (1/(50+t))*(pi1_i - pihat1_i)
+        pihat1_i += (1 / (50 + t)) * (pi1_i - pihat1_i)
 
-        price_i = gam + delta*f + alpha*((pi1_i + (1 - pi1_i)*pihat1_i)*beta1_i + (1 - pi1_i)*(1 - pihat1_i)*beta0_i +
-                (1 - pi1_i)*(1 - pihat1_i)*rho*sce*f) + vp
-        
-        # Store results in global arrays
-        price[i, t] = price_i
-        pi1[i, t] = pi1_i
-        pihat1[i, t] = pihat1_i
-        beta0[i, t] = beta0_i
-        beta1[i, t] = beta1_i
+        price_i = gam + delta * f + alpha * (
+            (pi1_i + (1 - pi1_i) * pihat1_i) * beta1_i +
+            (1 - pi1_i) * (1 - pihat1_i) * beta0_i +
+            (1 - pi1_i) * (1 - pihat1_i) * rho * sce * f
+        ) + vp
+
+        # Save results
+        price[i_sim, i_sf, i_sb, t] = price_i
+        pi1[i_sim, i_sf, i_sb, t] = pi1_i
+        pihat1[i_sim, i_sf, i_sb, t] = pihat1_i
+        beta0[i_sim, i_sf, i_sb, t] = beta0_i
+        beta1[i_sim, i_sf, i_sb, t] = beta1_i
     end
 end
 
-# **Setup Parameters**
-Nsim = 100  # Number of simulations
-Titer = 10000  # Number of time steps per simulation
-alpha = 0.96
-rho = 0.99
-sigmap = 0.2
-sigmaf = 0.0000000001
-sigmab = 0.00005;
+# Allocate 4D arrays
+price_gpu = CuArray(zeros(Float32, Nsim, Numf, Numb, Titer))
+pi1_gpu = CuArray(zeros(Float32, Nsim, Numf, Numb, Titer))
+pihat1_gpu = CuArray(zeros(Float32, Nsim, Numf, Numb, Titer))
+beta0_gpu = CuArray(zeros(Float32, Nsim, Numf, Numb, Titer))
+beta1_gpu = CuArray(zeros(Float32, Nsim, Numf, Numb, Titer))
 
-# **Allocate GPU Memory**
-price_gpu = CuArray(zeros(Nsim, Titer))
-pi1_gpu = CuArray(zeros(Nsim, Titer))
-pihat1_gpu = CuArray(zeros(Nsim, Titer))
-beta0_gpu = CuArray(zeros(Nsim, Titer))
-beta1_gpu = CuArray(zeros(Nsim, Titer))
-
-# **Generate Random Numbers on GPU Before Kernel Execution**
-vf_gpu = CuArray(randn(Nsim, Titer))  # Pre-generate vf values on GPU
-vp_gpu = CuArray(randn(Nsim, Titer));  # Pre-generate vp values on GPU
-
-# **Launch Kernel**
-threads_per_block = 256
-blocks = cld(Nsim, threads_per_block)
-@time @cuda threads=threads_per_block blocks=blocks compete_kernel!(price_gpu, pi1_gpu, pihat1_gpu, beta0_gpu, beta1_gpu, vf_gpu, vp_gpu, Nsim, Titer, alpha, rho, sigmap, sigmaf, sigmab)
+# Launch kernel
+threads = 256
+blocks = cld(TotalSim, threads)
+@time @cuda threads=threads blocks=blocks compete_kernel!(
+    price_gpu, pi1_gpu, pihat1_gpu, beta0_gpu, beta1_gpu,
+    sigmaf_grid, sigmab_grid,
+    Nsim, Numf, Numb, Titer,
+    alpha, rho, sigmap
+)
 
 # **Move Results Back to CPU**
 price = Array(price_gpu)
@@ -104,8 +142,8 @@ beta1 = Array(beta1_gpu);
 using Plots, LaTeXStrings
 # **Plot Results**
 pp = plot(layout=4)
-plot!(price[17,:], title="Price", legend=false, subplot=1)
-plot!(pihat1[17,:], title="\$ \\hat{\\pi}\$", legend=false,subplot=2)
-plot!(beta0[17,:], title="\$ \\beta_0 \$", legend=false,subplot=3)
-plot!(beta1[17,:], title="\$ \\beta_1 \$", legend=false,subplot=4)
-display(pp)
+plot!(price[1,1,1,:], title="Price", legend=false, subplot=1)
+plot!(pihat1[1,1,1,:], title="\$ \\hat{\\pi}\$", legend=false,subplot=2)
+plot!(beta0[1,1,1,:], title="\$ \\beta_0 \$", legend=false,subplot=3)
+plot!(beta1[1,1,1,:], title="\$ \\beta_1 \$", legend=false,subplot=4)
+savefig(pp,"compete_results.png")
--- Gresham codes/gresham4_julia_gpu.jl
+++ Gresham codes/gresham4_julia_gpu.jl
@@ -2,17 +2,32 @@
 ##    Written by Jung Jae Kim on March 24, 2025. GPU execution code of gresham4_julia_function.jl
 ##
 
+# Maximum bound of the simulation
+# Nsim = 40000, Titer = 10000
+
 using CUDA, Random, Distributions, Plots
 
-function gresham_kernel!(price, pi, beta0, beta1, vf_vals, vp_vals, Nsim, Titer, alpha, rho, sigmap, sigmaf, sigmab, delta, sce)
+# **Setup Parameters**
+Nsim = 10000  # Number of simulations
+Titer = 10000  # Number of time steps per simulation
+alpha = 0.96
+rho = 0.95
+sigmap = 1.0
+sigmaf = 1.0
+sigmab = 0.0005
+delta = (1 - rho * alpha)
+sce = delta / (1 - alpha * rho)
+
+
+function gresham_kernel!(price, pi, beta0, beta1, Nsim, Titer, alpha, rho, sigmap, sigmaf, sigmab, delta, sce)
     i = threadIdx().x + (blockIdx().x - 1) * blockDim().x  # Compute thread index
     if i > Nsim return end  # Ensure within range
 
     # Initialize per-simulation variables
     V0 = 0.2
     V1 = 0.2
-    beta0_i = sce + vp_vals[i, 1] * sqrt(V0)
-    beta1_i = sce + vp_vals[i, 2] * sqrt(V1)
+    beta0_i = sce + randn() * sqrt(V0)
+    beta1_i = sce + randn() * sqrt(V1)
     pi_i = 0.1
     r = 0.0
     price_i = 0.0
@@ -22,8 +37,8 @@ function gresham_kernel!(price, pi, beta0, beta1, vf_vals, vp_vals, Nsim, Titer,
     varbeta = (g * sigmap) / (sigmaf * (1 - alpha) * (2 - g * (1 - alpha)))
 
     for t = 2:Titer
-        vf = vf_vals[i, t]
-        vp = vp_vals[i, t]
+        vf = sqrt(sigmap) * randn()
+        vp = sqrt(sigmap) * randn()
         
         f = rho * f + vf
         res1 = price_i - beta1_i * f
@@ -51,31 +66,16 @@ function gresham_kernel!(price, pi, beta0, beta1, vf_vals, vp_vals, Nsim, Titer,
     end
 end
 
-# **Setup Parameters**
-Nsim = 4000  # Number of simulations
-Titer = 6000  # Number of time steps per simulation
-alpha = 0.96
-rho = 0.95
-sigmap = 1.0
-sigmaf = 1.0
-sigmab = 0.0005
-delta = (1 - rho * alpha)
-sce = delta / (1 - alpha * rho)
-
 # **Allocate GPU Memory**
 price_gpu = CuArray(zeros(Nsim, Titer))
 pi_gpu = CuArray(zeros(Nsim, Titer))
 beta0_gpu = CuArray(zeros(Nsim, Titer))
 beta1_gpu = CuArray(zeros(Nsim, Titer))
 
-# **Generate Random Numbers on GPU Before Kernel Execution**
-vf_gpu = CuArray(randn(Nsim, Titer))  # Pre-generate vf values on GPU
-vp_gpu = CuArray(randn(Nsim, Titer));  # Pre-generate vp values on GPU
-
 # **Launch Kernel**
 threads_per_block = 256
 blocks = cld(Nsim, threads_per_block)
-@time @cuda threads=threads_per_block blocks=blocks gresham_kernel!(price_gpu, pi_gpu, beta0_gpu, beta1_gpu, vf_gpu, vp_gpu, Nsim, Titer, alpha, rho, sigmap, sigmaf, sigmab, delta, sce)
+@time @cuda threads=threads_per_block blocks=blocks gresham_kernel!(price_gpu, pi_gpu, beta0_gpu, beta1_gpu, Nsim, Titer, alpha, rho, sigmap, sigmaf, sigmab, delta, sce)
 
 # **Move Results Back to CPU**
 price = Array(price_gpu)
@@ -83,9 +83,11 @@ pi = Array(pi_gpu)
 beta0 = Array(beta0_gpu)
 beta1 = Array(beta1_gpu);
 
+using Plots, LaTeXStrings
 # **Plot Results**
 plot1 = plot(price[1, :], label="Price")
 plot2 = plot(pi[1, :], ylim=(0, 1), label="Pi")
 plot3 = plot(beta0[1, :], label="Beta0")
 plot4 = plot(beta1[1, :], label="Beta1")
-plot(plot1, plot2, plot3, plot4, layout=(2, 2))
+pp=plot(plot1, plot2, plot3, plot4, layout=(2, 2))
+savefig(pp,"gresham_results.png")
