98fb784a64c8834012bb5d5cc77c68ee667d8635
rosalescristhian <cristhian.patricio.rosales.castillo@emory.edu>
2025-10-16 20:42:04 -0400

    myerson8_slurm.sh file updated - Cristhian

--- Myerson codes/2025/Cristhian/myerson8_slurm.sh
+++ Myerson codes/2025/Cristhian/myerson8_slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=0-3:00:00
 #SBATCH --mem=64G
 #SBATCH --nodes=5
-#SBATCH --ntasks-per-node=100
+#SBATCH --ntasks-per-node=50
 #SBATCH --job-name=myerson
 #SBATCH -o slurm.%N.%j.out # STDOUT
 #SBATCH -e slurm.%N.%j.err # STDERR
